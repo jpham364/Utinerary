8546700bfd72ad717a7a91286615ab26c770eb0a
Jonathan Pham <phamjonathan364@outlook.com>
2025-04-21 14:11:50 -0700

    Added base layout for plan details header

--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -45,23 +45,68 @@ export default function Plan() {
   return (
     <div className="min-h-screen p-8">
       <div className="max-w-6xl mx-auto">
+        
         {/* Header nav bar */}
         <Header/>
 
+        {/* Buttons  */}
         <div className="flex justify-start mt-4 mb-4">
-          
           <Button 
             variant="outline"
-            onClick={() => navigate(-1)}>
+            onClick={() => navigate("/home")}>
               <ChevronLeft />Back
           </Button>
-            
         </div>
 
+        {/* Title */}
         <h1 className="text-3xl font-bold mt-4">{plan.title}</h1>
-        <p className="text-muted-foreground">
-          Start Date: {plan.start ? format(new Date(plan.start), "PPP") : "No date"}
-        </p>
+
+        {/* Basic information */}
+        <div className="bg-muted rounded-2xl p-4 mt-4 space-y-4 text-muted-foreground shadow">
+          
+          {/* Header */}
+          <div className="flex justify-between items-center">
+            <h2 className="font-bold text-lg text-foreground">Details</h2>
+            <Button variant="default" size="sm" onClick={() => console.log("Open edit modal")}>
+              Edit
+            </Button>
+          </div>
+          
+
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
+
+            {/* Date */}
+            <div className="space-y-1">
+              <p className="text-foreground font-medium">Start Date</p>
+              <p>{plan.start ? format(new Date(plan.start), "PPP") : "No date"}</p>
+
+              <p className="text-foreground font-medium mt-2">End Date</p>
+              <p>{plan.end ? format(new Date(plan.end), "PPP") : "No date"}</p>
+            </div>
+
+            {/* Time */}
+            <div className="space-y-1">
+              <p className="text-foreground font-medium">Start Time</p>
+              <p>{plan.start ? format(new Date(plan.start), "p") : "No time"}</p>
+
+              <p className="text-foreground font-medium mt-2">End Time</p>
+              <p>{plan.end ? format(new Date(plan.end), "p") : "No time"}</p>
+            </div>
+
+            {/* Location */}
+            <div className="space-y-1">
+              <p className="text-foreground font-medium">Location</p>
+              <p>{plan.location || "No location set"}</p>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+
+
 
 
       </div>
